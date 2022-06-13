4af2fc94b387279239a3ed0007ab94d51bfba162
aenb04 <73962580+aenb04@users.noreply.github.com>
2022-06-13 13:35:33 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Halaman Login</title>
+
+    </head>
+    <body>
+                <div class="container" align="center" border="1">
+                    <h1>Login</h1>
+                        <form method="post" action="kliklogin.php">
+                            <label>username</label><br>
+                            <input type="text" name="namauser"><br>
+                            <label>Password</label><br>
+                            <input type="password" name="passworduser"><br>
+                            <p><button>Login</button></p>
+                        </form>
+                    </div>
+                            
+    </body>
+    </html>
